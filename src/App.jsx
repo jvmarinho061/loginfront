905c4f1608327721cce4c5e30a7f1899f635e978
fabio henrique <henri8274@gmail.com>
2024-11-25 21:41:38 -0300

    Criação de novos componentes: Cardapio.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from './Components/Login/Login'; 
 import Cadastro from './Components/Cadastro/Cadastro';
+import Cardapio from "./Components/Cardapio/Cardapio";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path= '/' element ={<Login/>}/>
+
+        <Route path= '/comida' element ={<Cardapio/>}/>
+
          <Route path= '/cadastro' element ={<Cadastro/>}/>
         <Route path= '*' element ={<h1>Not Found</h1>}/>
       </Routes>
---
+++ src/Components/Cardapio/Cardapio.jsx
@@ -0,0 +1,23 @@
+import './Cardapio.css'
+export default function Cardapio () {
+
+return (
+
+<div>
+
+<div  className='busca'>
+<input placeholder='Buscar' type="text"></input>
+</div>
+
+
+</div>
+
+)
+
+function andar (){
+
+    document.write("ola mundo");
+
+}
+
+}
