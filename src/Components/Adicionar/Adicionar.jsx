2f758a7f8675d56b1b7a2db60ec2b84f95f25ac3
Wilinton Gabriel <wilintongabrielss1@gmail.com>
2024-11-27 11:10:58 -0300

    Requisição POST criada

--- src/Components/Adicionar/Adicionar.jsx
+++ src/Components/Adicionar/Adicionar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Adicionar.css';
+import axios from 'axios';
 
 export default function Adicionar() {
 
@@ -11,23 +12,43 @@ export default function Adicionar() {
 
     function handleSubmit(event) {
         event.preventDefault();
+
         const dados = {
-            titulo: event.target.titulo.value,
-            urlImagem: event.target.urlImagem.value,
-            valor: event.target.valor.value,
-            estrelas: estrelas,
+            nome: event.target.nome.value,            
+            imgUrl: event.target.imgUrl.value,      
+            valor: event.target.valor.value,         
+            estrela: estrelas,                        
         };
+
+        if (!dados.nome || !dados.imgUrl || !dados.valor || !dados.estrela) {
+            console.error('Preencha todos os campos!');
+            return;
+        }
+
+        const url = 'http://localhost:8080/comida/adicionar';
+
+        function ok(response) {
+            console.log('Resposta do servidor:', response);
+            alert('Card Cadastrado com Sucesso');
+        }
+
+        axios.post(url, dados)
+            .then((response) => ok(response))
+            .catch((error) => {
+                console.error('Erro ao enviar os dados:', error);
+            });
 
         console.log("Dados a serem enviados:", dados);
     }
 
+
     return (
         <div className="adicionar-container">
-            <form className="adicionar-form">
+            <form className="adicionar-form" onSubmit={handleSubmit}>
                 <h2>Cadastrar Novo Card</h2>
                 <div className="inputs">
-                    <input type="text" name="titulo" placeholder="Título" required />
-                    <input type="text" name="urlImagem" placeholder="URL da Imagem" required />
+                    <input type="text" name="nome" placeholder="Título" required />
+                    <input type="text" name="imgUrl" placeholder="URL da Imagem" required />
                     <input type="number" name="valor" placeholder="Valor" required />
                     <div className="estrelas">
                         {[1, 2, 3, 4, 5].map((star) => (
@@ -40,6 +61,5 @@ export default function Adicionar() {
                 <button id="adicionar-form-btn" type="submit">Cadastrar</button>
             </form>
         </div>
-
     );
 }
